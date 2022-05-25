74f68711fe266411da03faeb4c47660847018f22
juanda hernandez <juandahernandez27991@gmail.com>
2022-05-24 19:46:00 -0500

    fixe db configuration

--- utils/database.js
+++ utils/database.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize');
 
 const db = new Sequelize({
   host: process.env.DB_HOST,
@@ -6,8 +6,17 @@ const db = new Sequelize({
   password: process.env.DB_PASSWORD,
   port: 5432,
   database: process.env.DB,
-  dialect: "postgres",
+  dialect: 'postgres',
   logging: false,
+  dialectOptions:
+    process.env.NODE_ENV === 'production'
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        }
+      : {}
 });
 
 module.exports = { db };
